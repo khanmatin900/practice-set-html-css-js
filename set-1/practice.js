13efd5b8e6e00862fe96b26a891c268edbbaaf43
khanmatin900 <matimkhan480@gmail.com>
2025-03-09 20:16:26 +0530

    add files

---
+++ set-1/practice.js
@@ -0,0 +1,34 @@
+  // practice Question-1
+
+  // let num = prompt("Enter a number:");
+  // if (num % 5 === 0) {
+  //   console.log(num, "is a multiple of 5");
+  // } else {
+  //   console.log(num, "'is Not a multiple of 5");
+  // }
+
+  
+ // practice Question-2
+
+  // let score = prompt("Enter your score (0-100):");
+  // let grade;
+  // if (score >= 90 && score <= 100) {
+  //   grade = "A";
+  // } else if (score >= 70 && score <= 89) {
+  //   grade = "B";
+  // } else if (score >= 60 && score <= 79) {
+  //   grade = "C";
+  // } else if (score >= 50 && score <= 59) {
+  //   grade = "D";
+  // } else if (score >= 0 && score <= 49 ){
+  //   grade = "F";
+  // }
+  // console.log("accordinto your scores, your grade was :", grade);
+
+const myDiv = document.querySelector(".main-2");
+let myAudio = document.querySelector('#audio');
+myAudio.play();
+
+myDiv.addEventListener('click', function() {
+myDiv.move = myAudio;
+})
